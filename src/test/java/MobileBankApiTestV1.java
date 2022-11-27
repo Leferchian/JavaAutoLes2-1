0b674c67c8cfa5f6d157819ccefd4f5cfbbed81d
Egor Boyko <leferchian@gmail.com>
2022-11-27 20:15:18 +0300

    add Schema

--- src/test/java/MobileBankApiTestV1.java
+++ src/test/java/MobileBankApiTestV1.java
@@ -1,6 +1,8 @@
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.*;
 
 class MobileBankApiTestV1 {
     @Test
@@ -15,5 +17,21 @@ class MobileBankApiTestV1 {
                 // Проверки
                 .then()
                 .statusCode(200);
+    }
+
+    @Test
+    void shouldMatchJSONSchema() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                .then()
+                .statusCode(200)
+                // static import для JsonSchemaValidator.matchesJsonSchemaInClasspath
+                .body(matchesJsonSchemaInClasspath("accounts.schema.json"));
     }
 }
